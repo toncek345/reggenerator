3e7c19aee0fad05cbe891342840c3510f0f217d3
Antonio Bosak <toni.bosak@gmail.com>
2022-11-20 16:33:56 +0100

    fix special char

--- toknizer_test.go
+++ toknizer_test.go
@@ -88,10 +88,7 @@ func TestTokenize(t *testing.T) {
 					charRange: "-",
 				},
 				{
-					charRange: "?",
-				},
-				{
-					charRange: ".",
+					charRange: "?.",
 				},
 				{
 					charRange: ",",
